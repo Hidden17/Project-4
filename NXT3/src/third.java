74cc7e68b49f827bf1af932f1c8292657109f94a
Hidden17 <theexmod@gmail.com>
2017-06-20 15:37:14 +0200

    Add files via upload

---
+++ NXT3/src/third.java
@@ -0,0 +1,759 @@
+import lejos.nxt.Motor;
+import lejos.nxt.Button;
+
+public class third {
+	private static int counter10=2;
+	private static int counter20=2;
+	private static int counter50=2;
+	static boolean alwaysTrue = true;
+	
+	public static void main(String[] args) {
+//    Motor.A.forward();
+//    Button.waitForAnyPress();
+//		Motor.A.rotate(-1000);
+//		Motor.A.setSpeed(300);
+//		MotorA();
+//		MotorB();
+//		MotorC();
+
+		try{
+		if (alwaysTrue = true){
+			System.out.println("Still running");
+			if(counter10 ==1){
+				MotorA();
+			}
+			
+			if(counter20 ==1){
+				MotorB();
+			}
+			
+			if(counter50 ==1){
+				MotorC();
+			}
+//			
+//			if(counter10 ==1 && counter20==0 && counter50==0){
+//				MotorA();
+//			}
+//			
+//			if(counter10 ==0 && counter20==1 && counter50==0){
+//				MotorB();
+//			}
+//			
+//			if(counter10 ==0 && counter20==0&& counter50==1){
+//				MotorC();
+//			}
+////			Hier begint 2 biljetten combinaties
+//			if(counter10 ==2 && counter20==0 && counter50==0){
+//				MotorA();
+//				MotorA(); //20
+//			}
+//			
+//			
+//			if(counter10 ==0 && counter20==2 && counter50==0){
+//				MotorB();
+//				MotorB(); //40
+//			}
+//			
+//			if(counter10 ==0 && counter20==0&& counter50==2){
+//				MotorC();//100
+//				MotorC();
+//			}
+//			
+//			if(counter10 ==1 && counter20==1 && counter50==0){
+//				MotorA();
+//				MotorB(); //30
+//			}
+//			
+//			
+//			if(counter10 ==1 && counter20==0 && counter50==1){
+//				MotorA();
+//				MotorC(); //60
+//			}
+//			
+//			if(counter10 ==0 && counter20==1&& counter50==1){
+//				MotorB();//100
+//				MotorC();
+//			}
+//			
+////			Hier begint 3 biljetten combinaties
+//			if(counter10 ==3 && counter20==0 && counter50==0){
+//				MotorA();
+//				MotorA(); //30
+//				MotorA();
+//			}
+//			
+//			if(counter10 ==0 && counter20==3 && counter50==0){
+//				MotorB();
+//				MotorB(); //60
+//				MotorB();
+//			}
+//			
+//			if(counter10 ==0 && counter20==0&& counter50==3){
+//				MotorC();//150
+//				MotorC();
+//				MotorC();
+//			}
+//			
+//			if(counter10 ==1 && counter20==1 && counter50==1){
+//				MotorA();
+//				MotorB(); //80
+//				MotorC();
+//			}
+//			
+//			if(counter10 ==2 && counter20==1 && counter50==0){
+//				MotorA();
+//				MotorA();
+//				MotorB(); //40
+//			}
+//			
+//			if(counter10 ==2 && counter20==0 && counter50==1){
+//				MotorA();
+//				MotorA();
+//				MotorC(); //70
+//			}
+//			
+//			if(counter10 ==1 && counter20==2 && counter50==0){
+//				MotorA();
+//				MotorB();
+//				MotorB(); //50
+//			}			
+//			
+//			if(counter10==0 && counter20==2 && counter50==1){
+//				MotorB();
+//				MotorB();
+//				MotorC(); //90
+//			}
+//			
+//			if(counter10==0 && counter20==1 && counter50==2){
+//				MotorB();
+//				MotorC();
+//				MotorC(); //120
+//			}
+//			
+//			if(counter10==1 && counter20==0 && counter50==2){
+//				MotorA();
+//				MotorC();
+//				MotorC(); //110
+//			}
+			if(counter10==2){
+				MotorA();
+				MotorA();
+			}
+			if(counter10==3){
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==4){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==5){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==6){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==7){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==8){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==9){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==10){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==11){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==12){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==13){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==14){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==15){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==16){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==17){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==18){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==19){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+			if(counter10==20){
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+				MotorA();
+			}
+
+			
+			if(counter20 ==2){
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter20 ==3){
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter20 ==4){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter20 ==5){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter20 ==6){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter20 ==7){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter20 ==8){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter20 ==9){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter20 ==10){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==11){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==12){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==13){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==14){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==15){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==16){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==17){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==18){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==19){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			if(counter20 ==20){
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+				MotorB();
+			}
+			
+			if(counter50==2){
+				MotorC();
+				MotorC();
+			}
+			if(counter50==3){
+				MotorC();
+				MotorC();
+				MotorC();
+			}
+			if(counter50==4){
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+			}
+			if(counter50==5){
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+			}
+			if(counter50==6){
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+			}
+			if(counter50==7){
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+			}
+			if(counter50==8){
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+			}
+			if(counter50==9){
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+			}
+			if(counter50==10){
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+				MotorC();
+			}
+		}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static void MotorA(){
+		Motor.A.rotate(-500);
+		Motor.A.setSpeed(1000);
+		Motor.A.rotate(250);
+		
+	}
+	public static void MotorB(){
+		Motor.B.rotate(-500);
+		Motor.B.setSpeed(1000);
+		Motor.B.rotate(250); 
+	}
+	public static void MotorC(){
+		Motor.C.rotate(-350);
+		Motor.C.setSpeed(500);
+		Motor.C.rotate(250);
+	}
+	
+	public static void KeepGoing(){
+		
+	}
+	public void setCounter(int counter) {
+		this.counter10 = counter;
+	}
+	public int getCounter() {
+		return counter10;
+	}
+	public void setCounter20(int counter20) {
+		this.counter20 = counter20;
+	}
+	public int getCounter20() {
+		return counter20;
+	}
+	public void setCounter50(int counter50) {
+		this.counter50 = counter50;
+	}
+	public int getCounter50() {
+		return counter50;
+	}
+}
